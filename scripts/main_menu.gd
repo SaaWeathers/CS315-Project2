87824daa118712eeaa070cab5b10166497294cbe
Saa Weathers <11saaweathers5@gmail.com>
2025-10-25 19:29:00 -0400

    Main Menu Commit
    1st draft of main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Level_1.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
