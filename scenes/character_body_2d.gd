d19b7af1de6cb577ef8bcd4e801ccb3078b86fe6
Saa Weathers <11saaweathers5@gmail.com>
2025-10-27 14:22:39 -0400

    Started Level 1
    Collect food cans as a cat to get to the next level. The bed will be a save

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
 
+var facing = 1
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	#get direction of movement
+	
+	var dir = Input.get_vector("left","right","up","down")
+	
+	self.velocity = dir * SPEED
+	
+	if dir.x < 0 and facing == 1:
+		self.scale.x *= -1
+		facing = -1
+		$AnimatedSprite2D.play("walk")
+	
+	if dir.x > 0 and facing == -1:
+		self.scale.x *= -1
+		facing = 1
+		$AnimatedSprite2D.play("walk")
+	if dir.is_zero_approx():
+		$AnimatedSprite2D.play("idle")
+		pass
+	
+	
 	move_and_slide()
