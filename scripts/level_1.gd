b847f31682e996e03276c0666114350ad8c90d14
Saa Weathers <11saaweathers5@gmail.com>
2025-10-30 01:52:23 -0400

    Cans UI and Health UI
    - Can total now shows up and goes down by each can collected. Still needs work done
    - Health now is displayed and properly goes down with the change_health function

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,14 +1,14 @@
 extends Node
 
 var total_cans = 0
-
 var collected_cans = 0
+var health = 5
 
-var health = 3
-@onready var health_bar = $ProgressBar
+@onready var health_bar = $UI/ProgressBar
+@onready var cans_left = $"UI/Number of cans"
 
 var in_bed = false
-
+var temp_cans = total_cans
 
 func _ready() -> void:
 	#count cans in the scene automatically based on how many I add to scene
@@ -16,31 +16,39 @@ func _ready() -> void:
 	print("Cans: ", total_cans)
 
 func _process(delta: float) -> void:
+	
+	cans_left.text = ": %d" % [temp_cans]
+	
 	if Input.is_action_pressed("use") and in_bed == true and collected_cans == total_cans:
-				print("change level")
-				level_change()
+			print("change level")
+			level_change()
 
 
 
-
-
-func add_can():
+func add_cans():
 	collected_cans += 1
 	print("Cans collected: %d / %d" % [collected_cans, total_cans])
 
+func change_cans_left():
+	temp_cans -= 1
+	print(cans_left)
+	cans_left.text = ": %d" % [total_cans]
+	
+#for when the cat runs into things that hurt it
 func change_health():
 	health -= 1
 	health_bar.value = health
 	print(health)
 
+#only works when cat is in the bed
 func level_change():
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	
 
 func _on_food_body_entered(body: Node2D) -> void:
-	add_can()
+	add_cans()
+	change_cans_left()
 	$Food/Meow.play()
-	change_health()
 	pass
 
 
